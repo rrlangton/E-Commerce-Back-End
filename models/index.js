79cb46d3b90a39e17e96ea9fe078f4e27802515c
Robin <rrlangton@gmail.com>
2024-04-05 19:23:21 -0500

    index.js added

--- models/index.js
+++ models/index.js
@@ -7,10 +7,30 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 
 // Categories have many Products
+Category.belongsToMany(Product , {
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'Product categories'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(ProductTag, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+  as: 'Products with tags'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: {
+    model: Tag,
+    unique: false
+  },
+});
 
 module.exports = {
   Product,
